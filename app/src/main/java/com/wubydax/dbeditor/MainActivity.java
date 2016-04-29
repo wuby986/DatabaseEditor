489531054ae26d9630f308a4dcf6c2bd4b45386a
wuby986 <roberto.mariani3@gmail.com>
2016-04-29 10:59:39 +0200

    remove also FAB is actually useless

--- app/src/main/java/com/wubydax/dbeditor/MainActivity.java
+++ app/src/main/java/com/wubydax/dbeditor/MainActivity.java
@@ -1,7 +1,6 @@
 package com.wubydax.dbeditor;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -9,7 +8,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 /*      Created by Roberto Mariani and Anna Berkovitch, 26/03/16
         This program is free software: you can redistribute it and/or modify
@@ -57,15 +55,6 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         assert tabLayout != null;
         tabLayout.setupWithViewPager(viewPager);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        assert fab != null;
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                recreate();
-            }
-        });
 
     }
 
